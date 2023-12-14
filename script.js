02e50109fce986432ac3897c0b9590816f0a495d
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-12-14 15:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+
+	const counterElement = docoment.getElementById('counter');
+	let score = 0;
+
+    function incrementCounter() {
+    	   alert(`Current Counter Value: ${score}`);
+
+		score++;
+		counterElement.textContent = score;
+    }
