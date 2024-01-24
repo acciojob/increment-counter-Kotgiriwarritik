68bc1b75fb7e7b1481ac1555e788470aa94908f6
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2024-01-24 17:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //your JS code here. If required.
+const myButton = document.getElementById("incrementBtn");
+const myPara =  document.getElementById("counter");
 
+let normalCount = 0;
 
-	const counterElement = docoment.getElementById('counter');
-	let score = 0;
-
-    function incrementCounter() {
-    	   alert(`Current Counter Value: ${score}`);
-
-		score++;
-		counterElement.textContent = score;
-    }
+function incrementNum() {
+    normalCount++;
+	myPara.innerText = normalCount;
+}
+myButton.addEventListener("click",()=>{
+	incrementNum();
+});
